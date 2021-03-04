e1bb0dfd56d0cf6b897b276b26fd133a66828936
Lyra Brooke Brown <73356104+LyraMakes@users.noreply.github.com>
2021-03-04 18:48:02 -0500

    Reindented to 4

--- web_host.py
+++ web_host.py
@@ -5,28 +5,31 @@ app = Flask('')
 
 numServers = 8
 
+
 def set_num_servers(n: int) -> type(None):
-  global numServers
-  numServers = n
+    global numServers
+    numServers = n
+
 
 def get_num_servers() -> int:
-  global numServers
-  return numServers
+    global numServers
+    return numServers
+
 
 @app.route('/')
 def home():
-  return 'temp'
+    return 'temp'
 
 
 @app.route('/ping')
 def ping():
-  return 'Current status: Up'
+    return 'Current status: Up'
 
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
