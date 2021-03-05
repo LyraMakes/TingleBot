0420c823fdfa0fced98595e342da2d0e5b7b1d06
Lyra Brooke Brown <73356104+LyraMakes@users.noreply.github.com>
2021-03-05 18:32:46 -0500

    Changed default page

--- web_host.py
+++ web_host.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('')
 
-numServers = 8
+numServers = 0
 
 
+# Getters and setters
 def set_num_servers(n: int) -> type(None):
     global numServers
     numServers = n
@@ -16,20 +17,21 @@ def get_num_servers() -> int:
     return numServers
 
 
+# Web app routes
 @app.route('/')
-def home():
-    return 'temp'
+def home() -> str:
+    return render_template('index.html', guilds=get_num_servers())
 
 
 @app.route('/ping')
-def ping():
+def ping() -> str:
     return 'Current status: Up'
 
 
-def run():
+def run() -> type(None):
     app.run(host='0.0.0.0', port=8080)
 
 
-def keep_alive():
+def keep_alive() -> type(None):
     t = Thread(target=run)
     t.start()
